599c7506e47afa203ddb0c61adb07f950c0a5a04
Himanshu Wakodikar <115973209+himwak2001@users.noreply.github.com>
2026-01-08 05:46:07 +0000

    feat: :sparkles: add Koko Eating Bananas to Medium class

--- src/binary-search/Medium.java
+++ src/binary-search/Medium.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Medium{
     /*
      * method to find the peak element
@@ -19,13 +21,41 @@ public class Medium{
         return -1;
     }
 
+    /*
+     * method to minimum integer k such that Koko can eat all the bananas within h hours.
+     * TC - O(logn) * O(n)
+     * SC - O(1)
+     */
+    private int calculateTotalHours(int[] piles, int hour){
+        int totalHours = 0;
+        for(int pile : piles){
+            totalHours += Math.ceil((double)pile / (double)hour);
+        }
+        return totalHours;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 0, ans = 0;
+        int high = Arrays.stream(piles).max().getAsInt();
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(calculateTotalHours(piles, mid) <= h){
+                ans = mid;
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
 
     /*
      * main method to get the output
      */
     public static void main(String[] args) {
         Medium medium = new Medium();
-        int[] nums = {1,2,1,3,5,6,4};
-        System.out.println(medium.findPeakElement(nums));
+        int[] nums = {30,11,23,4,20};
+        System.out.println(medium.minEatingSpeed(nums, 6));
     }
 }
