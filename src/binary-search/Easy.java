19409c4cff345a9fc03ea60947e0e284fae90ec3
Himanshu Wakodikar <115973209+himwak2001@users.noreply.github.com>
2026-01-09 06:47:55 +0000

    feat: :sparkles: add method to find lower and upper bound in Easy Class

--- src/binary-search/Easy.java
+++ src/binary-search/Easy.java
@@ -1,4 +1,45 @@
 public class Easy {
+    /**
+     * method to find lower bound / search insert position
+     * TC - O(logn)
+     * SC - O(1)
+     */
+    private int lowerBound(int[] arr, int target){
+        int n = arr.length;
+        int low = 0, high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    /**
+     * method to find upper bound
+     * TC - O(logn)
+     * SC - O(1)
+     */
+    private int upperBound(int[] arr, int target){
+        int n = arr.length;
+        int low = 0, high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid] > target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+
     /*
      * method to find square root of an integer
      * TC - O(logn)
@@ -22,6 +63,8 @@ public class Easy {
      */
     public static void main(String[] args) {
         Easy easy = new Easy();
-        System.out.println(easy.floorSqrt(11));
+        int[] arr = {2, 3, 7, 10, 11, 11, 25};
+        // System.out.println(easy.lowerBound(arr, 100));
+        System.out.println(easy.upperBound(arr, 11));
     }
 }
