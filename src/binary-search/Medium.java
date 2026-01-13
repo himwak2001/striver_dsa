e395617f08fb9d6a87db6b2f82803a07e112c6aa
Himanshu Wakodikar <115973209+himwak2001@users.noreply.github.com>
2026-01-13 05:54:29 +0000

    feat: :sparkles: Add method search target in rotated sorted array containing unique elements

--- src/binary-search/Medium.java
+++ src/binary-search/Medium.java
@@ -14,7 +14,7 @@ public class Medium {
             if (nums[mid] == target) {
                 first = mid;
                 high = mid - 1;
-            } else if(nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
@@ -70,6 +70,37 @@ public class Medium {
                 low = mid + 1;
             else
                 high = mid - 1;
+        }
+        return -1;
+    }
+
+    /**
+     * method to search target in rotated sorted array contain unique elements
+     * TC - O(logn)
+     * SC - O(1)
+     */
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] < nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         return -1;
     }
@@ -306,11 +337,11 @@ public class Medium {
      */
     public static void main(String[] args) {
         Medium medium = new Medium();
-        int[] nums = { 5, 7, 7, 8, 8, 10 };
-        // System.out.println(medium.aggressiveCows(nums, 3));
-        int[] output = medium.searchRange(nums, 6);
-        for(int num : output){
-            System.out.println(num);
-        }
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(medium.search(nums, 3));
+        // int[] output = medium.searchRange(nums, 6);
+        // for (int num : output) {
+        // System.out.println(num);
+        // }
     }
 }
